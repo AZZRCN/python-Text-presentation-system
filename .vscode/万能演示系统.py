9d6a136171f85422dd3f7bbadcd023e8efdd81c9
AZZRCN <qazwsx233343@163.com>
2023-05-20 01:27:50 +0800

    v1.0.3
    能够选取路径

--- .vscode/万能演示系统.py
+++ .vscode/万能演示系统.py
@@ -4,14 +4,22 @@ from os import chdir
 from tkinter.filedialog import askdirectory
 from tkinter.messagebox import showerror
 main = tk.Tk()
-
-
+main.attributes("-fullscreen",True)
+namelist=[]
 def Esc():
     global text
     text.destroy()
     text = tk.Text(master=main,width=200)
 text = tk.Text(master=main,width=200)
-num = 0
+def newpathc():
+    global path
+    path = askdirectory()
+    for i in namelist:
+        i.destory()
+    frush(path)
+newpath = tk.Button(master=main,text="NEW PATH",fg="orange",command=newpathc)
+newpath.grid(column=0,row=0)
+num = 1
 def tkopen(filename):
     f = 0
     try:
@@ -53,13 +61,14 @@ def frush(path):
             continue
         if (i[-4:-1] + i[-1] == ".txt"):  # and i[0:3] == "演示-"
             print(i)
-            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +
-                 "\",command=lambda:tkopen(\""+i+"\"))\ntemp"+str(num)+".grid(row="+str(num)+", column=0)")
+            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +"\",command=lambda:tkopen(\""+i+"\"))\n\
+                 temp"+str(num)+".grid(row="+str(num)+", column=0)\n\
+                 global namelist\n\
+                 namelist.append(temp)")
             num += 1
 
 path = askdirectory()
 if path != "":frush(path)
-main.attributes("-fullscreen",True)
 main.update()
 h=main.winfo_height()
 w=main.winfo_width()
