0526da757cf0897e65956f1a8d25e48d58b457de
AZZRCN <qazwsx233343@163.com>
2023-05-21 11:30:00 +0800

    v1.0.4
    优化代码

--- .vscode/万能演示系统.py
+++ .vscode/万能演示系统.py
@@ -15,36 +15,28 @@ def newpathc():
     global path
     path = askdirectory()
     for i in namelist:
-        i.destory()
+        i.destroy()
     frush(path)
 newpath = tk.Button(master=main,text="NEW PATH",fg="orange",command=newpathc)
 newpath.grid(column=0,row=0)
 num = 1
+
 def tkopen(filename):
     f = 0
-    try:
-        f = open(filename, encoding="utf-8")
-    except BaseException:
+    encodinglist=["utf-8", "gbk","gb2312","ANSI","UTF16"]
+    for i in encodinglist:
         try:
-            f = open(filename, encoding="gbk")
+            f = open(filename, encoding=i)
         except BaseException:
-            try:
-                f = open(filename, encoding="gb2312")
-            except BaseException:
-                try:
-                    f = open(filename, encoding="Unicode")
-                except BaseException:
-                    try:
-                        f = open(filename, encoding="ANSI")
-                    except BaseException:
-                        try:
-                            f = open(filename, encoding="USC2")
-                        except BaseException:
-                            try:
-                                f = open(filename, encoding="UTF16")
-                            except BaseException:
-                                showerror(title="错误!",message="这个文件可能不是正规txt?")
-                                return
+            continue
+        else:
+            break
+    try:
+        f.read()
+    except BaseException:
+         showerror(title="错误!",message="这个文件可能不是正规txt?")
+         return
+    f.seek(0)
     global text
     text.grid(column=1,row=0,rowspan=num)
     text.insert('1.0', str(f.read()))
@@ -57,14 +49,10 @@ def frush(path):
     global num
     for i in list_a:
         if (len(i) < 4):
-
             continue
         if (i[-4:-1] + i[-1] == ".txt"):  # and i[0:3] == "演示-"
             print(i)
-            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +"\",command=lambda:tkopen(\""+i+"\"))\n\
-                 temp"+str(num)+".grid(row="+str(num)+", column=0)\n\
-                 global namelist\n\
-                 namelist.append(temp)")
+            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +"\",command=lambda:tkopen(\""+i+"\"))\ntemp"+str(num)+".grid(column=0,row="+str(num)+")\nglobal namelist\nnamelist.append(temp" + str(num) + ")")
             num += 1
 
 path = askdirectory()
