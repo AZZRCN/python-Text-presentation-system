cd45f19c16e46f0d82d1fd4fdf7013b16520adba
AZZRCN <qazwsx233343@163.com>
2023-05-21 15:51:58 +0800

    v1.0.5
    优化逻辑

--- .vscode/万能演示系统.py
+++ .vscode/万能演示系统.py
@@ -5,59 +5,34 @@ from tkinter.filedialog import askdirectory
 from tkinter.messagebox import showerror
 main = tk.Tk()
 main.attributes("-fullscreen",True)
-namelist=[]
-def Esc():
-    global text
-    text.destroy()
-    text = tk.Text(master=main,width=200)
-text = tk.Text(master=main,width=200)
-def newpathc():
-    global path
-    path = askdirectory()
-    for i in namelist:
-        i.destroy()
-    frush(path)
-newpath = tk.Button(master=main,text="NEW PATH",fg="orange",command=newpathc)
-newpath.grid(column=0,row=0)
+main.update()
+h,w=main.winfo_height(),main.winfo_width()
+buttonlist=[]
+text = tk.Text(master=main)
 num = 1
-
+def newpathc():
+    for i in buttonlist:
+        i.destroy()
+    frush(askdirectory())
+tk.Button(master=main,text="NEW PATH",fg="orange",command=newpathc).grid(column=0,row=0)
 def tkopen(filename):
-    f = 0
-    encodinglist=["utf-8", "gbk","gb2312","ANSI","UTF16"]
-    for i in encodinglist:
+    text.delete('1.0','end')
+    for i in ["utf-8", "gbk","gb2312","ANSI","UTF16"]:
         try:
-            f = open(filename, encoding=i)
+            text.insert('1.0', str(open(filename, encoding=i).read()))
         except BaseException:
             continue
         else:
             break
-    try:
-        f.read()
-    except BaseException:
-         showerror(title="错误!",message="这个文件可能不是正规txt?")
-         return
-    f.seek(0)
-    global text
-    text.grid(column=1,row=0,rowspan=num)
-    text.insert('1.0', str(f.read()))
-    f.close()
-
+    if(text.get('1.0','end') == ""):showerror(title="错误!",message="这个文件可能不是正规txt?")
 def frush(path):
     global main
     chdir(path)
-    list_a = listdir()
     global num
-    for i in list_a:
-        if (len(i) < 4):
-            continue
-        if (i[-4:-1] + i[-1] == ".txt"):  # and i[0:3] == "演示-"
-            print(i)
-            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +"\",command=lambda:tkopen(\""+i+"\"))\ntemp"+str(num)+".grid(column=0,row="+str(num)+")\nglobal namelist\nnamelist.append(temp" + str(num) + ")")
+    for i in listdir():
+        if (i[-4:-1] + i[-1] == ".txt" or i[-3:-1] + i[-1] == ".py"):
+            exec("temp"+str(num)+" = tk.Button(master=main, text=\""+i +"\",font=(\"等线\",10),command=lambda:tkopen(\""+i+"\"))\ntemp"+str(num)+".grid(column=0,row="+str(num)+")\nglobal buttonlist\nbuttonlist.append(temp" + str(num) + ")")
             num += 1
-
-path = askdirectory()
-if path != "":frush(path)
-main.update()
-h=main.winfo_height()
-w=main.winfo_width()
+frush(askdirectory())
+text.grid(column=1,row=0,rowspan=num+1)
 main.mainloop()
